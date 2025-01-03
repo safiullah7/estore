ccd29421f98235c2620030133e02025d41765c09
Safi Ullah <iamsafiullah@gmail.com>
2025-01-03 17:14:22 +0500

    fontawesome and icons

--- estore/src/app/header/header.component.ts
+++ estore/src/app/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  protected readonly faSearch = faSearch;
 }
